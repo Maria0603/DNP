2e593468d81f77ae6be9244c3d9da6eee30ad71e
Mariia Moskovko <39185269+Maria0603@users.noreply.github.com>
2024-09-20 11:13:39 +0200

    Assignment3 (#3)
    * Assignment3 deploy (#2)
    
    * update_gitignore
    
    * Update .gitignore
    
    * business_logic_added
    
    * UI added
    
    * gitignore+ui
    
    * Update .gitignore
    
    * clean gitignore
    
    * Update .gitignore
    
    * file repository added
    
    * deleting stuff
    
    * deleting more stuff
    
    * Update Program.cs

---
+++ FileRepositories/CommentFileRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using Entities;
+using RepositoryContracts;
+
+namespace FileRepositories;
+
+public class CommentFileRepository : BaseFileRepository<Comment>, ICommentRepository {
+    public CommentFileRepository() : base("comments.json") {
+        if (!File.Exists("comments.json")) {
+            var dummyComments = new List<Comment> {
+                new Comment ( 1, "First Comment", 1 , 1),
+                new Comment ( 2, "Second Comment", 2 , 2)
+            };
+            string commentsJson = JsonSerializer.Serialize(dummyComments);
+            File.WriteAllText("comments.json", commentsJson);
+        }
+    }
+}
---
+++ FileRepositories/UserFileRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using Entities;
+using RepositoryContracts;
+
+namespace FileRepositories;
+
+public class UserFileRepository : BaseFileRepository<User>, IUserRepository {
+    public UserFileRepository() : base("users.json") {
+        if (!File.Exists("users.json")) {
+            var dummyUsers = new List<User> {
+                new User ( 1, "John Doe", "john.doe@example.com" ),
+                new User ( 2, "Jane Smith", "jane.smith@example.com" )
+            };
+            string usersJson = JsonSerializer.Serialize(dummyUsers);
+            File.WriteAllText("users.json", usersJson);
+        }
+    }
+}
