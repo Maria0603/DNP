ee7e4db33689965905f9c27be3897b741bbdee54
Mariia Moskovko <39185269+Maria0603@users.noreply.github.com>
2024-09-20 11:11:18 +0200

    Assignment3 deploy (#2)
    * update_gitignore
    
    * Update .gitignore
    
    * business_logic_added
    
    * UI added
    
    * gitignore+ui
    
    * Update .gitignore
    
    * clean gitignore
    
    * Update .gitignore
    
    * file repository added
    
    * deleting stuff
    
    * deleting more stuff

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using CLI.UI;
+
 using InMemoryRepositories;
 using RepositoryContracts;
 
--- CLI/UI/ManagePosts/AddCommentView.cs
+++ CLI/UI/ManagePosts/AddCommentView.cs
@@ -7,6 +7,7 @@ public class AddCommentView(IUserRepository userRepository, IPostRepository post
     public async Task AddAsync() {
         Console.WriteLine("Enter comment content:");
         var content = Console.ReadLine();
+
         Console.WriteLine("Enter comment author ID:");
         var userId = int.Parse(Console.ReadLine());
         var author = await userRepository.GetSingleAsync(userId);
@@ -14,15 +15,19 @@ public class AddCommentView(IUserRepository userRepository, IPostRepository post
             Console.WriteLine("Author not found.");
             return;
         }
+
         Console.WriteLine("Enter post ID:");
         var postId = int.Parse(Console.ReadLine());
+
         var post = await postRepository.GetSingleAsync(postId);
         if (post == null) {
             Console.WriteLine("Post not found.");
             return;
         }
+
         var comment = new Comment(commentRepository.GetMany().Count() + 1, content, userId, postId);
         await commentRepository.AddAsync(comment);
+
         Console.WriteLine("Comment created successfully");
     }
     
